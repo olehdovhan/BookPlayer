d92cdbe3beec897eaaf0d1f63d7cd538caf9fc92
Oleh Dovhan <olehdovhan@Olehs-MacBook-Air.local>
2023-12-18 16:10:43 +0200

    Add gitignore file

---
+++ BookPlayer/BookPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPlayerApp.swift
+//  BookPlayer
+//
+//  Created by Oleh Dovhan on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
